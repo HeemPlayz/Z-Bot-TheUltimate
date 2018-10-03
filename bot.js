f9a8b5b34429444271ed9bbf69debdc8bd5e9c31
_ShaDoW_MaN_ <32039975+ShadwX57@users.noreply.github.com>
2018-10-03 20:27:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,7 @@ var jimp = require('jimp')
 const prefix = "p#";
 let done = {};
 
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   console.log('')
@@ -42,6 +43,318 @@ client.on('ready', () => {
   console.log('')
 });
 
+const profile = JSON.parse(fs.readFileSync('./profile.json', "utf8"));
+
+client3.on("message", message => {
+  if (message.author.bot) return;
+  if(!message.channel.guild)return;
+  if (!profile[message.author.id]) profile[message.author.id] = {
+    info: 'p#inforprofile To Set The Info',
+    rep: 0,
+    reps: 'NOT YET',
+    lastDaily:'Not Collected',
+    level: 0,
+    points: 0,
+  };
+fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
+if (err) console.error(err);
+})
+});
+//لايك//
+client3.on('message', message => {
+  if (message.author.bot) return;
+    var sender = message.author
+    if (message.author.id === client3.user.id) return;
+  if(!message.channel.guild) return;
+    if(message.content.startsWith(prefix + 'لايك')) {
+    let ment = message.mentions.users.first()
+if (games[sender.id].lastDaily != moment().format('day')) {
+    games[sender.id].lastDaily = moment().format('day')
+        if(!ment) return message.channel.send(`**:mag: |  ${message.author.username}, the user could not be found.    **`);
+        if(ment = message.author.id) return message.channel.send(`**${message.author.username}, you cant give yourself a reputation !**`)
+    profile[ment.id].rep += 1;
+    message.channel.send(`** :up:  |  ${message.author.username} has given ${ment} a reputation point!**`)
+    }else {
+    message.channel.send(`**:stopwatch: |  ${message.author.username}, you can award more reputation  ${moment().endOf('day').fromNow()} **`)
+    }
+
+    }
+    });
+
+
+
+//هدية//
+client3.on("message", (message) => {
+  var sender = message.author
+if(message.content.startsWith(prefix + 'daily')) {
+if (games[sender.id].lastDaily != moment().format('day')) {
+    games[sender.id].lastDaily = moment().format('day')
+ games[message.author.id].credits += 200;
+    message.channel.send(`**${message.author.username} you collect your \`200\` :dollar: daily pounds**`)
+} else {
+    message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
+}
+}
+})
+//مصاري//
+client3.on("message", (message) => {
+  if (message.author.bot) return;
+    if (message.author.id === client3.user.id) return;
+  if(!message.channel.guild) return;
+if (message.content === '=credits') {
+message.channel.send(`** ${message.author.username}, your :credit_card: balance is ${games[message.author.id].credits}.**`)
+}
+});
+//معلوماتي
+client3.on('message', message => {
+  if (message.author.bot) return;
+    if (message.author.id === client3.user.id) return;
+  if(!message.channel.guild) return;
+        if(message.content.startsWith('p#inforprofile')) {
+        let args = message.content.split(' ').slice(1).join(' ')
+        if(!args) return message.channel.send(`**${message.author.username}, يرجى كتابة المعلومات**`)
+        if(args.length > 25) return message.channel.send(`**${message.author.username} يجب ان لا تكون المعلومات اكثر من 25 حرف**`)
+        profile[message.author.id].info = args
+        message.channel.send(`**${message.author.username}**| تم تغير معلوماتك الى  =${args}>`)
+    }
+});
+//لفل
+client3.on('message', message => {
+  if (message.author.bot) return;
+    if (message.author.id === client3.user.id) return;
+  if(!message.channel.guild) return;
+var sender = message.author;
+const games =profile;
+games[sender.id].points += 1;
+if (!profile[sender.id].points) profile[sender.id].points= 0 ;
+if (!profile[sender.id].level) profile[sender.id].level= 0 ;
+if (profile[sender.id].points == 50) profile[sender.id].level = 1 ;
+
+if (profile[sender.id].points == 120) profile[sender.id].level = 2;
+
+if (profile[sender.id].points == 260) profile[sender.id].level = 3;
+
+if (profile[sender.id].points == 400) profile[sender.id].level = 4;
+
+if (profile[sender.id].points == 560) profile[sender.id].level = 5;
+
+if (profile[sender.id].points == 780) profile[sender.id].level = 6;
+
+if (profile[sender.id].points == 900) profile[sender.id].level = 7;
+
+if (profile[sender.id].points == 1100) profile[sender.id].level = 8;
+
+if (profile[sender.id].points == 1350) profile[sender.id].level = 9;
+
+if (profile[sender.id].points == 1700) profile[sender.id].level = 10;
+
+if (profile[sender.id].points == 2100) profile[sender.id].level = 11;
+
+if (profile[sender.id].points == 2300) profile[sender.id].level = 12;
+
+if (profile[sender.id].points == 2500) profile[sender.id].level = 13;
+
+if (profile[sender.id].points == 2800) profile[sender.id].level = 14;
+
+if (profile[sender.id].points == 3200) profile[sender.id].level = 15;
+
+if (profile[sender.id].points == 3600) profile[sender.id].level = 16;
+
+if (profile[sender.id].points == 4000) profile[sender.id].level = 17;
+
+if (profile[sender.id].points == 4500) profile[sender.id].level = 18;
+
+if (profile[sender.id].points == 5000) profile[sender.id].level = 19;
+
+if (profile[sender.id].points == 5700) profile[sender.id].level = 20;
+
+if (profile[sender.id].points == 6200) profile[sender.id].level = 21;
+
+if (profile[sender.id].points == 6800) profile[sender.id].level = 22;
+
+if (profile[sender.id].points == 7500) profile[sender.id].level = 23;
+
+if (profile[sender.id].points == 8500) profile[sender.id].level = 24;
+
+if (profile[sender.id].points == 9600) profile[sender.id].level = 25;
+
+if (profile[sender.id].points == 11000) profile[sender.id].level = 26;
+
+if (profile[sender.id].points == 12500) profile[sender.id].level = 27;
+
+if (profile[sender.id].points == 14000) profile[sender.id].level = 28;
+
+if (profile[sender.id].points == 16000) profile[sender.id].level = 29;
+
+if (profile[sender.id].points == 18500) profile[sender.id].level = 30;
+
+if (profile[sender.id].points == 20000) profile[sender.id].level = 31;
+
+if (profile[sender.id].points == 22000) profile[sender.id].level = 32;
+
+if (profile[sender.id].points == 24500) profile[sender.id].level = 33;
+
+if (profile[sender.id].points == 27000) profile[sender.id].level = 34;
+
+if (profile[sender.id].points == 30000) profile[sender.id].level = 35;
+
+if (profile[sender.id].points == 33000) profile[sender.id].level = 36;
+
+if (profile[sender.id].points == 36000) profile[sender.id].level = 37;
+
+if (profile[sender.id].points == 40000) profile[sender.id].level = 38;
+
+if (profile[sender.id].points == 45000) profile[sender.id].level = 39;
+
+if (profile[sender.id].points == 50000) profile[sender.id].level = 40;
+
+if (profile[sender.id].points == 56000) profile[sender.id].level = 41;
+
+if (profile[sender.id].points == 61000) profile[sender.id].level = 42;
+
+if (profile[sender.id].points == 68000) profile[sender.id].level = 43;
+
+if (profile[sender.id].points == 75000) profile[sender.id].level = 44;
+
+if (profile[sender.id].points == 83000) profile[sender.id].level = 45;
+
+if (profile[sender.id].points == 90000) profile[sender.id].level = 46;
+
+if (profile[sender.id].points == 95000) profile[sender.id].level = 47;
+
+if (profile[sender.id].points == 100000) profile[sender.id].level = 48;
+
+if (profile[sender.id].points == 106000) profile[sender.id].level = 49;
+
+if (profile[sender.id].points == 111000) profile[sender.id].level = 50;
+
+});
+//**بروفايل**//
+client3.on("message", message => {
+  if (message.author.bot) return;
+  if(!message.channel.guild) return;
+if (message.content.startsWith("p#profile")) {
+                               let user = message.mentions.users.first();
+         var men = message.mentions.users.first();
+            var heg;
+            if(men) {
+                heg = men
+            } else {
+                heg = message.author
+            }
+          var mentionned = message.mentions.members.first();
+             var h;
+            if(mentionned) {
+                h = mentionned
+            } else {
+                h = message.member
+            }
+            var ment = message.mentions.users.first();
+            var getvalueof;
+            if(ment) {
+              getvalueof = ment;
+            } else {
+              getvalueof = message.author;
+            }//var ghost = tf 3lek xD
+   var mentionned = message.mentions.users.first();
+
+    var client3;
+      if(mentionned){
+          var client3 = mentionned;
+      } else {
+          var client3 = message.author;
+
+      }
+  const w = ['./PicsArt_08-28-06.21.07.png'];
+if (!games[getvalueof.id]) games[getvalueof.id] = {wins: 0,loses: 0,points: 0,total: 0,credits: 100,level: 1,};
+            let Image = Canvas.Image,
+            canvas = new Canvas(300, 300),
+            ctx = canvas.getContext('2d');
+      fs.readFile(`${dataPro[getvalueof.id].wallSrc}`, function (err, Background) {
+          fs.readFile(`${w[0]}`, function (err, Background) {
+          if (err) return console.log(err);
+          let BG = Canvas.Image;
+          let ground = new Image;
+          ground.src = Background;
+          ctx.drawImage(ground, 0, 0, 297, 305);
+});
+          if (err) return console.log(err);
+          let BG = Canvas.Image;
+          let ground = new Image;
+          ground.src = Background;
+          ctx.drawImage(ground, 0, 0, 300, 305);
+});
+
+
+
+                let url = getvalueof.displayAvatarURL.endsWith(".webp") ? getvalueof.displayAvatarURL.slice(5, -20) + ".png" : getvalueof.displayAvatarURL;
+                jimp.read(url, (err, ava) => {
+                    if (err) return console.log(err);
+                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                        if (err) return console.log(err);
+
+
+                        //Avatar
+                       let Avatar = Canvas.Image;
+                        let ava = new Avatar;
+                        ava.src = buf;
+                     ctx.drawImage(ava, 8, 43, 80, 85); // احداثيات صورتك
+
+                        //ur name
+                        ctx.font = 'bold 16px Arial'; // حجم الخط و نوعه
+                        ctx.fontSize = '40px'; // عرض الخط
+                        ctx.fillStyle = "#000000"; // لون الخط
+                        ctx.textAlign = "left"; // محاذا ة النص
+                        ctx.fillText(`${getvalueof.username}`, 130, 125) // احداثيات اسمك
+
+                         //bord
+                        ctx.font = "regular 12px Cairo" // نوع الخط وحجمه
+                        ctx.fontSize = '50px'; // عرض الخط
+                        ctx.fillStyle = "#f0ff00" // لون الخط
+                        ctx.textAlign = "left"; // محاذا ة النص
+                        ctx.fillText(`Soon...`, 170, 198) // احداثيات ترتيبك
+
+                        //credit
+                        ctx.font = "bold 10px Arial" // نوع الخط وحجمه
+                        ctx.fontSize = '10px'; // عرض الخط
+                        ctx.fillStyle = '#FFFFFF' // لون الخط
+                        ctx.textAlign = "left"; // محاذا ة النص
+                        ctx.fillText(`$ ${games[getvalueof.id].credits}`, 156, 163) // احداثيات المصاري
+
+                        //poits
+                        ctx.font = "bold 13px Arial" // ن
+                        ctx.fontSize = '10px'; // عرض الخطوع الخط وحجمه
+                        ctx.fillStyle = "#FFFFFF" // لون الخط
+                        ctx.textAlign = "left"; // محاذا ة النص
+                        ctx.fillText(`${profile[getvalueof.id].points}`, 173, 182) // احداثيات النقاط
+
+                        //Level
+                        ctx.font = "bold 27px Arial" // نوع الخط و حجمه
+                        ctx.fontSize = '50px'; // عرض الخط
+                        ctx.fillStyle = "#FFFFFF" // لون الخط
+                        ctx.textAlign = "left"; // محاذا ة النص
+                        ctx.fillText(`${profile[getvalueof.id].level}`, 30, 200) // احداثيات اللفل
+
+                        //info
+                        ctx.font = "blod 13px Arial" // ن
+                        ctx.fontSize = '10px'; // عرض الخطوع الخط وحجمه
+                        ctx.fillStyle = "#FFFFFF" // لون الخط
+                        ctx.textAlign = "left"; // محاذا ة النص
+                        ctx.fillText(`${profile[getvalueof.id].info}`, 118, 40) // احداثيات النقاط
+
+                        // REP
+                        ctx.font = "bold 27px Arial";
+                        ctx.fontSize = "100px";
+                        ctx.fillStyle = "#FFFFFF";
+                        ctx.textAlign = "left";
+                        ctx.fillText(`+${profile[getvalueof.id].rep}`, 18,270)
+
+message.channel.sendFile(canvas.toBuffer())
+})
+})
+}
+
+});
 
 
 client.on("message", message => {
@@ -2869,7 +3182,7 @@ if (err) console.error(err);
  });
 
  client3.on("message", message => {
-  if (message.content === "p#welcome") {
+  if (message.content === "p#help-welcome") {
    const embed = new Discord.RichEmbed()
        .setColor("RANDOM")
          .setDescription('👑أوامر الترحيب👑')
@@ -2889,7 +3202,7 @@ if (err) console.error(err);
    if (message.author.bot) return;
    if(!message.channel.guild)return;
    if (!profile[message.author.id]) profile[message.author.id] = {
-     info: '=inforprofile To Set The Info',
+     info: 'p#inforprofile To Set The Info',
      rep: 0,
      reps: 'NOT YET',
      lastDaily:'Not Collected',
@@ -3073,7 +3386,7 @@ if (err) console.error(err);
  client3.on("message", message => {
    if (message.author.bot) return;
    if(!message.channel.guild) return;
- if (message.content.startsWith("=profile")) {
+ if (message.content.startsWith("p#profile")) {
                                 let user = message.mentions.users.first();
           var men = message.mentions.users.first();
              var heg;
