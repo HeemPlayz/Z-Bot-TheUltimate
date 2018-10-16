cb074ce1c058d583857bf943771cfe8037a908e6
_ShaDoW_MaN_ <32039975+ShadwX57@users.noreply.github.com>
2018-10-16 15:33:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,11 +33,11 @@ if (message.content ===  'd!color 140'){
 if(!message.channel.guild) return;
 if (message.member.hasPermission('MANAGE_ROLES')){
   setInterval(function(){})
+  let role7
     let count = 0;
     let ecount = 0;
 for(let x = 1; x < 141; x++){
-message.guild.deleteRole({name:x,
-color: 'RANDOM'})
+role7.delete()
 }
 }
 }
