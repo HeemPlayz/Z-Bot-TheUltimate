1299940df886e19ec20e71fa3a7b51273e11c473
Discord.js Arabic Developer <32039975+ShadwX57@users.noreply.github.com>
2018-10-20 20:22:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,6 +25,44 @@ const prefix = "d!";
 let done = {};
 const Token = process.env.BOT_TOKEN
 
+client.on('message', async message => {
+  let mention = message.mentions.members.first();
+let command = message.content.split(" ")[0];
+   command = command.slice(prefix.length);
+  let args = message.content.split(" ").slice(1);	 
+	if (message.content === "d!unmute") {
+    if(!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.sendMessage("**You Donot HavePermission Mute_Members**").then(m => m.delete(5000));
+if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return message.reply("**I donot Have Permission Mute_Members**").then(msg => msg.delete(6000))
+
+  let kinggamer = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+     if(!kinggamer) return message.channel.send('').then(msg => {
+      msg.delete(3500);
+      message.delete(3500); 
+    });
+
+  let role = message.guild.roles.find (r => r.name === "Muted");
+  
+  if(!role || !kinggamer.roles.has(role.id)) return message.channel.sendMessage(`**:information_source:${mention.user.username} لقد تم فك الميوت عنه مسبقا**`)
+
+  await kinggamer.removeRole(role) 
+  message.channel.sendMessage(`**:white_check_mark: ${mention.user.username}  Unmuted! **`);
+  let mutedEmbed = new Discord.RichEmbed()
+.setDescription("New UnMute User")
+.setThumbnail(message.guild.iconURL)
+.setColor("#bc0000")
+.addField("Unmuted", `${Warned} with ID ${Warned.id}`)
+.addField("Unmuted By", `<@${message.member.id}> with ID ${message.member.id}`)
+.addField("Unmuted In", message.channel)
+.addField("Time & Date", `${message.createdAt}`)
+.setFooter("DragonBot 🐲")
+let incidentchannel = message.guild.channels.find(`name`, "incidents");
+if(!incidentchannel) return message.channel.send("Can't find incidents channel.");
+
+  return;
+
+  }
+
+});
 
 client.on('message', async message =>{
     if (message.author.boss) return;  
@@ -32,8 +70,8 @@ client.on('message', async message =>{
       let command = message.content.split(" ")[0];
        command = command.slice(prefix.length);
       let args = message.content.split(" ").slice(1);
-      if (command == "mute") {
-          if (!message.channel.guild) return;
+      if (message.content === "d!mute") {
+        if (!message.channel.guild) return;
           if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.reply("You Dont Have Permissions To Do That :x:").then(msg => msg.delete(5000));
           if(!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) return message.reply("I Dont Have Permissions To Do That :x:").then(msg => msg.delete(5000));;
           let user = message.mentions.users.first();
@@ -48,7 +86,7 @@ client.on('message', async message =>{
           .addField('- Muted By:',message.author,true)
           .addField('- Muted User:', `${user}`)
           .addField('- Reason:',reason,true)
-          .setFooter(message.author.username,message.author.avatarURL);
+          .setFooter("DragonBot 🐲")
           let incidentchannel = message.guild.channels.find(`name`, "incidents");
           if(!incidentchannel) return message.channel.send("Can't find incidents channel.");
           incidentchannel.sendEmbed(muteEmbed)
@@ -583,44 +621,7 @@ User.send(`**:airplane: You are has been banned in ${message.guild.name} reason:
 
 
 
-client.on('message', async message => {
-  let mention = message.mentions.members.first();
-let command = message.content.split(" ")[0];
-   command = command.slice(prefix.length);
-  let args = message.content.split(" ").slice(1);	 
-if(command === `unmute`) {2
-  if(!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.sendMessage("**You Donot HavePermission Mute_Members**").then(m => m.delete(5000));
-if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return message.reply("**I donot Have Permission Mute_Members**").then(msg => msg.delete(6000))
 
-  let kinggamer = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-     if(!kinggamer) return message.channel.send('').then(msg => {
-      msg.delete(3500);
-      message.delete(3500); 
-    });
-
-  let role = message.guild.roles.find (r => r.name === "Muted");
-  
-  if(!role || !kinggamer.roles.has(role.id)) return message.channel.sendMessage(`**:information_source:${mention.user.username} لقد تم فك الميوت عنه مسبقا**`)
-
-  await kinggamer.removeRole(role) 
-  message.channel.sendMessage(`**:white_check_mark: ${mention.user.username}  Unmuted! **`);
-  let mutedEmbed = new Discord.RichEmbed()
-.setDescription("New UnMute User")
-.setThumbnail(message.guild.iconURL)
-.setColor("#bc0000")
-.addField("Unmuted", `${Warned} with ID ${Warned.id}`)
-.addField("Unmuted By", `<@${message.member.id}> with ID ${message.member.id}`)
-.addField("Unmuted In", message.channel)
-.addField("Time & Date", `${message.createdAt}`)
-.setFooter("DragonBot 🐲")
-let incidentchannel = message.guild.channels.find(`name`, "incidents");
-if(!incidentchannel) return message.channel.send("Can't find incidents channel.");
-
-  return;
-
-  }
-
-});
 client.on('message', message => {
   var prefix = "d!";
     if (message.author.kick) return;
