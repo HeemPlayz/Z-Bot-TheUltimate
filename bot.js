0df09a639e90ad6f40a7bace1961bf9e99465089
Discord.js Arabic Developer <32039975+ShadwX57@users.noreply.github.com>
2018-10-20 20:25:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ let command = message.content.split(" ")[0];
 if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return message.reply("**I donot Have Permission Mute_Members**").then(msg => msg.delete(6000))
 
   let kinggamer = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-     if(!kinggamer) return message.channel.send('').then(msg => {
+     if(!kinggamer) return message.channel.send('Mention Someone').then(msg => {
       msg.delete(3500);
       message.delete(3500); 
     });
