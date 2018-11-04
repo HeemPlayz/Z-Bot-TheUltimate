9e2f247885ab06a8bdc516eb382abded0eb6fe6d
Discord.js Arabic Developer <32039975+Bowlingtoolkit@users.noreply.github.com>
2018-11-04 12:07:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,6 +29,46 @@ const prefix = ">";
 let done = {};
 const Token = process.env.BOT_TOKEN
 
+client.on('message', message => {
+    if(!message.channel.guild) return;
+if(message.content.startsWith('>bc')) {
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let copy = "KingBot 👑";
+let request = `Requested By ${message.author.username}`;
+if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+msg.react('✅')
+.then(() => msg.react('❌'))
+.then(() =>msg.react('✅'))
+
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+message.channel.send(`☑ |   ${message.guild.members.size} يتم ارسال البرودكاست الى عضو `).then(m => m.delete(5000));
+message.guild.members.forEach(m => {
+var bc = new
+Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle(':mega: برودكاست')
+.addField('🔰السيرفر🔰', message.guild.name)
+.addField('🚩المرسل🚩', message.author.username)
+.addField('📜الرسالة📜', args)
+.setThumbnail('https://a.top4top.net/p_1008gqyyd1.png')
+.setFooter(copy, client.user.avatarURL);
+m.send({ embed: bc })
+msg.delete();
+})
+})
+reaction2.on("collect", r => {
+message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+msg.delete();
+})
+})
+}
+})
 
 
 client.on('message', async message => {
@@ -189,46 +229,6 @@ client.on('message',async message => {
       },1000);
     });
     }
-  })
-  client.on('message', message => {
-            if(!message.channel.guild) return;
-  if(message.content.startsWith('>bc')) {
-  if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-  let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-  let copy = "KingBot 👑";
-  let request = `Requested By ${message.author.username}`;
-  if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-  msg.react('✅')
-  .then(() => msg.react('❌'))
-  .then(() =>msg.react('✅'))
-
-  let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-  let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-     let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-  let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-  reaction1.on("collect", r => {
-  message.channel.send(`☑ |   ${message.guild.members.size} يتم ارسال البرودكاست الى عضو `).then(m => m.delete(5000));
-  message.guild.members.forEach(m => {
-var bc = new
-Discord.RichEmbed()
-.setColor('RANDOM')
-.setTitle(':mega: برودكاست')
-.addField('🔰السيرفر🔰', message.guild.name)
-.addField('🚩المرسل🚩', message.author.username)
-.addField('📜الرسالة📜', args)
-.setThumbnail('https://a.top4top.net/p_1008gqyyd1.png')
-.setFooter(copy, client.user.avatarURL);
-  m.send({ embed: bc })
-  msg.delete();
-  })
-  })
-  reaction2.on("collect", r => {
-  message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
-  msg.delete();
-  })
-  })
-  }
   })
 
 
