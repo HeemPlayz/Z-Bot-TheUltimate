101c35a11f33c416c4d6d94b9e803bb7835fa1eb
Discord.js Arabic Developer <32039975+Bowlingtoolkit@users.noreply.github.com>
2018-11-05 20:52:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6018,30 +6018,49 @@ client.on("guildMemberAdd", member => {
     welcomer.sendFile(canvas.toBuffer())
 
 
-        if (member.id == client.user.id) {
-            return;
-        }
-        var guild;
-        while (!guild)
-        guild.fetchInvites().then((data) => {
-            
-            data.forEach((Invite, key, map) => {
-                var Inv = Invite.code;
-                if (dat[Inv])
-                    if (dat[Inv] < Invite.uses) {
-                        setTimeout(function() {
-     welcomer.send(`**invited by** ${Invite.inviter} `) ;
-                        },1500);
-     }
-                dat[Inv] = Invite.uses;
-           
-           });
+});
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
         });
-     });
-    })
-   })
-  }
- })
+    });
+});
+ 
+ 
+ 
+client.on("guildMemberAdd", (member) => {
+    if (member.id == client.user.id) {
+        return;
+    }
+    var guild;
+    while (!guild)
+    guild.fetchInvites().then((data) => {
+        
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    setTimeout(function() {
+ welcomer.send(`**invited by** ${Invite.inviter} `) ;
+                    },1500);
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+ });
+})
+})
+}
+})
 
 client.on('message', message => {
 
