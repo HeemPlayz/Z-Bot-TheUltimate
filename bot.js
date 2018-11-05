28459260573577eb3c864623bceb6f8ca32c562c
Discord.js Arabic Developer <32039975+Bowlingtoolkit@users.noreply.github.com>
2018-11-05 20:35:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,16 +29,7 @@ const prefix = ">";
 let done = {};
 const Token = process.env.BOT_TOKEN
 
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "welcome");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  }); 
-});
+
 client.on('message', message => {
     if(!message.channel.guild) return;
 if(message.content.startsWith('>bc')) {
@@ -6027,12 +6018,31 @@ client.on("guildMemberAdd", member => {
     welcomer.sendFile(canvas.toBuffer())
 
 
+        if (member.id == client.user.id) {
+            return;
+        }
+        var guild;
+        while (!guild)
+        guild.fetchInvites().then((data) => {
+            data.forEach((Invite, key, map) => {
+                var Inv = Invite.code;
+                if (dat[Inv])
+                    if (dat[Inv] < Invite.uses) {
+                        setTimeout(function() {
+     welcomer.send(`**invited by** ${Invite.inviter} `) ;
+                        },1500);
+     }
+                dat[Inv] = Invite.uses;
+           
+           });
+        });
+    });
 
       })
       })
 
       }
-     )}
+    })
 
 
 
@@ -6889,7 +6899,7 @@ if (err) console.error(err);
         }
       };
       });
-    }})})
+    }})
     
 
    
